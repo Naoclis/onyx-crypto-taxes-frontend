a5bbabdf46eaf8adbc9bb0e750750b7f46e0107d
Naoclis <dev@naoclis.com>
2022-09-16 21:30:25 +0200

    Fin de la recette de l'export, je suis globalement rassuré mais faudra repasser dessus

--- src/components/Taxes/OldAppTesting/ExportUzan.tsx
+++ src/components/Taxes/OldAppTesting/ExportUzan.tsx
@@ -77,7 +77,7 @@ const AggRow = (props: any) => {
             status += 100;
             diff.source = compar.sourceOpposite[0].qty - item.qty;
 
-            message += ` | Dans la clé opposée Source : diff = ${diff.source.toFixed(3)}`;
+            message += ` | Dans la clé opposée Source`;
         }
 
 
@@ -116,6 +116,8 @@ const AggRow = (props: any) => {
             <TableRow>
                 <TableCell>{item.asset}</TableCell>
                 <TableCell>{item.qty}</TableCell>
+                <TableCell>{compar.sourceOpposite.length > 0 && Math.abs(item.qty - compar.sourceOpposite[0].qty).toFixed(2)}</TableCell>
+                <TableCell>{compar.sameKey.length > 0 && compar.sameKey[0].qty.toFixed(2)}</TableCell>
                 <TableCell>{item.date.length}</TableCell>
             </TableRow>
 
@@ -127,33 +129,16 @@ const AggRow = (props: any) => {
 const AggBox = (props: any) => {
     const { source, _key, testing } = props;
 
-    const findTestingAggRow = (arr: any, item: any) => {
-        const hasTestingRow = arr.filter((el: any) => el.asset === item.asset);
-        if (hasTestingRow.length === 1) {
-            const diff = item.qty - hasTestingRow[0].qty;
-            const test = (Math.abs(diff) < 0.0001);
-
-            return hasTestingRow[0];
-        }
-        else {
-            return null;
-        }
-    };
-
     const hasTestingAggRow = (arr: any, item: any) => {
+        console.log(item);
         let test = false;
-        const hasTestingRow = arr.filter((el: any) => el.asset === item.asset);
+        const hasTestingRow = arr.filter((el: any) => el.asset.toLowerCase() === item.asset.toLowerCase());
         if (hasTestingRow.length === 1) {
             const diff = item.qty - hasTestingRow[0].qty;
             test = (Math.abs(diff) < 0.0001);
             if (!test) {
                 //Je vérifie si la diff n'est pas de l'autre côté
                 test = checkOppositeSide(item);
-                if (item.asset === 'FIL') {
-                    console.log(item);
-                    console.log(hasTestingRow[0]);
-                    console.log(test);
-                }
             }
         }
         else {
@@ -165,9 +150,9 @@ const AggBox = (props: any) => {
 
     const checkOppositeSide = (item:any) => {
         const opposite = (_key === 'outputs') ? 'inputs' : 'outputs';
-        const testSrc = testing[_key].filter((el: any) => el.asset === item.asset);
-        const testOpp = testing[opposite].filter((el: any) => el.asset === item.asset);
-        const sourceOpp = source[opposite].filter((el: any) => el.asset === item.asset);
+        const testSrc = testing[_key].filter((el: any) => el.asset.toLowerCase() === item.asset.toLowerCase());
+        const testOpp = testing[opposite].filter((el: any) => el.asset.toLowerCase() === item.asset.toLowerCase());
+        const sourceOpp = source[opposite].filter((el: any) => el.asset.toLowerCase() === item.asset.toLowerCase());
         const testOppQty = (testOpp.length > 0) ? testOpp[0].qty : 0;
         const testSrcQty = (testSrc.length > 0) ? testSrc[0].qty : 0;
         const sourceOppQty = (sourceOpp.length > 0) ? sourceOpp[0].qty : 0;
@@ -186,12 +171,14 @@ const AggBox = (props: any) => {
                 <TableHead>
                     <TableRow>
                         <TableCell>Asset</TableCell>
-                        <TableCell>Qté totale</TableCell>
+                        <TableCell>Qté</TableCell>
+                        <TableCell>Qté totale (input+output)</TableCell>
+                        <TableCell>Cible à trouver</TableCell>
                         <TableCell>Nb Date</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {source !== undefined && source[_key].map((item: any, index: number) => (
+                    {source !== undefined && source[_key].filter((el: any) => ['TWT', 'GAS', 'BNB'].includes(el.asset) === false).map((item: any, index: number) => (
                         <React.Fragment key={index}>
                             {!hasTestingAggRow(testing[_key], item) && <AggRow item={item} _key={_key} testing={testing} source={source}/>}
                         </React.Fragment>
@@ -204,51 +191,11 @@ const AggBox = (props: any) => {
     )
 };
 
-
-const Row = (props: any) => {
-    const { item } = props;
-
-    return (
-        <React.Fragment>
-            <TableCell>{item.date}</TableCell>
-            <TableCell>{item.exchange}</TableCell>
-            <TableCell>{item.outputAsset}</TableCell>
-            <TableCell>{item.outputValue}</TableCell>
-            <TableCell>{item.inputAsset}</TableCell>
-            <TableCell>{item.inputValue}</TableCell>
-            <TableCell>{item.feeAsset}</TableCell>
-            <TableCell>{item.feeValue}</TableCell>
-        </React.Fragment>
-    )
-};
-
-const TestingRow = (props: any) => {
-    const { item } = props;
-
-    return (
-        <TableRow className="odd">
-            <Row item={item} />
-        </TableRow>
-    )
-};
-
-const ExportRow = (props: any) => {
-    const { item } = props;
-
-    return (
-        <TableRow>
-            <Row item={item} />
-        </TableRow>
-    )
-};
-
 const ExportUzan = () => {
     //Variables
     const apiCaller = new ApiOperations();
 
     //States
-    const [exported, setExported] = useState([]);
-    const [testing, setTesting] = useState([]);
     const [aggExported, setAggExported] = useState({ fees: [], outputs: [], inputs: [] });
     const [aggTesting, setAggTesting] = useState({ fees: [], outputs: [], inputs: [] });
     const [inProgress, setInProgress] = useState<number>(-1);
@@ -257,51 +204,12 @@ const ExportUzan = () => {
         setInProgress(1);
         const res = await apiCaller.get('get/assetsEvolution/forLawyer', 'taxesCalculator');
         if (res !== undefined) {
-            const { dataExported, testingData, aggExports, testAggExports } = res;
-            setTesting(testingData);
-            setExported(dataExported);
+            const { aggExports, testAggExports } = res;
             setAggExported(aggExports);
             setAggTesting(testAggExports);
-            //findNotMatchingRow(dataExported, testingData);
         }
         setInProgress(-1);
     };
-
-    const findNotMatchingRow = (exported: any, testing: any) => {
-        const missingRows: any = [];
-        for (const item of exported) {
-            const { date, outputAsset } = item;
-            const hasTestingRow = testing.filter((el: any) => el.date.substring(0, 10) === date.substring(0, 10)
-                && el.outputAsset === outputAsset
-                /*&& el.inputAsset === item.inputAsset 
-                && el.feeAsset === item.feeAsset*/
-            );
-            if (hasTestingRow.length < 1) {
-                missingRows.push(item);
-            }
-        }
-        setExported(missingRows);
-    };
-
-
-    const findTestingRowForDate = (item: any) => {
-        const hasTestingRow = testing.filter((el: any) => el.date === item.date
-            && el.outputAsset === item.outputAsset
-            /*&& el.inputAsset === item.inputAsset 
-            && el.feeAsset === item.feeAsset*/
-        );
-        if (hasTestingRow.length === 1) {
-            return hasTestingRow[0];
-        }
-        else if (hasTestingRow.length > 1) {
-            console.log('More than 1');
-            return hasTestingRow[0];
-        }
-        else {
-            console.log('Nope');
-            return null;
-        }
-    }
 
 
     //Effects
@@ -322,37 +230,6 @@ const ExportUzan = () => {
                 <Box mb={2}>
                     <Loader message="Action en cours" />
                 </Box>
-            }
-            {inProgress === 2 &&
-                <Table sx={defaultStyles.table}>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Date</TableCell>
-                            <TableCell>Plateforme d'échange ou autre</TableCell>
-                            <TableCell>Monnaie tradée</TableCell>
-                            <TableCell>Montant tradé</TableCell>
-                            <TableCell>Monnaie reçue</TableCell>
-                            <TableCell>Montant reçu</TableCell>
-                            <TableCell>Monnaie frais</TableCell>
-                            <TableCell>Montant frais</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {exported.filter((el: any) => !['Kucoin', 'HardWalletNeo', 'HardWalletMain', 'HardWalletSecond'].includes(el.exchange) && el.date <= '2021-09-21 20:30:01').map((item: any, index: number) => (
-                            <React.Fragment key={index}>
-                                <ExportRow item={item} />
-                                {findTestingRowForDate(item) !== null ? <TestingRow item={findTestingRowForDate(item)} /> :
-                                    <TableRow className="even">
-                                        <TableCell colSpan={8}>-</TableCell>
-                                    </TableRow>
-                                }
-
-                            </React.Fragment>
-                        )
-
-                        )}
-                    </TableBody>
-                </Table>
             }
             {inProgress !== 1 &&
                 <Box>
